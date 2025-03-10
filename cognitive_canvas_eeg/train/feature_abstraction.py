cfc2f2ef860390032d468c8e0011907bd03df123
lizocf <lizelle.ocfemia@cooper.edu>
2025-03-10 15:56:42 -0400

    try modeling

--- cognitive_canvas_eeg/train/feature_abstraction.py
+++ cognitive_canvas_eeg/train/feature_abstraction.py
@@ -19,6 +19,9 @@ app = typer.Typer()
 
 # TODO: make variable names consistent 
 
+FEATURE_NAMES = ["hjorth_activity", "hjorth_mobility", "hjorth_complexity", "katz_FD" ]
+
+
 class FeatureAbstraction():
     def __init__(self,n,input_path: Path, output_path: Path):
         self.principal=PCA(n_components=n)
@@ -26,12 +29,12 @@ class FeatureAbstraction():
         self.output_path = output_path
 
     def get_eeg_df(self, csv_path):
-        subject_df = pd.read_csv(csv_path, header=1)
-        return subject_df[EEG_COLUMNS]
+        subject_df = pd.read_csv(csv_path)
+        return subject_df[EEG_COLUMNS], subject_df['Label']
 
-    def hjort_params(self,df_eeg):
+    def hjorth_params(self,df_eeg):
         activity = np.var(df_eeg.to_numpy().T,axis=-1)
-        mobility, complexity = ant.hjorth_params(self.df_eeg.to_numpy().T)
+        mobility, complexity = ant.hjorth_params(df_eeg.to_numpy().T)
 
         return activity,mobility,complexity
 
@@ -43,29 +46,48 @@ class FeatureAbstraction():
         return self.principal.transform(x)
     
     def batches(self):
-        feature_df = pd.DataFrame(columns=['hjorth_activity', 'hjorth_mobility', 'hjorth_complexity','katz_FD','pca','Label'])
-        for csv in os.listdir(self.input_path):
+        feature_df = pd.DataFrame(columns=['hjorth_activity', 'hjorth_mobility', 'hjorth_complexity','katz_FD', 'Label'])
+        big_out = []
+        for csv in tqdm(os.listdir(self.input_path)):
+            # print(csv)
             if csv.endswith(".csv") and csv.startswith("raw"):
                 csv_path = os.path.join(self.input_path, csv)
 
-                try:
-                    eeg_df = self.get_eeg_df(csv_path)
-                    components = self.pca(eeg_df)
-                    activity,mobility,complexity = self.hjort_params(eeg_df)
-                    fractals = self.katz_fractals(eeg_df)
+                eeg_df, label = self.get_eeg_df(csv_path)
+                components = self.pca(eeg_df)
+                activity, mobility, complexity = self.hjorth_params(eeg_df)
+                fractals = self.katz_fractals(eeg_df)
 
-                    feature_df = feature_df.append({'hjorth_activity': activity, 'hjorth_mobility': mobility, 'hjorth_complexity' : complexity,'katz_FD' : fractals,'pca': components,'Label' : eeg_df['Label']}, ignore_index=True)
+                data = ([activity, mobility, complexity, fractals])
 
-                except:
-                    pass 
+                out = []
+                for i, d in enumerate(data):
+                    list_name = [FEATURE_NAMES[i] + "_" + s for s in EEG_COLUMNS]
+                    out.append(dict(zip(list_name, d)))
 
-        feature_df.to_csv(f"{self.output_path}/{csv}", index=False)
+                big_out.append(out[0] | out[1] | out[2] | out[3] | {'Label': label[0]})
 
+
+
+
+
+
+
+
+                # breakpoint()
+
+                # temp_df = pd.DataFrame({'hjorth_activity': activity, 'hjorth_mobility': mobility, 
+                #                         'hjorth_complexity' : complexity,'katz_FD' : fractals,
+                #                         'Label' : label[:14]})
+                # feature_df = pd.concat([feature_df, temp_df], ignore_index=True)
+
+                # breakpoint()
         
+        feature_df = pd.DataFrame(big_out)
 
+        breakpoint()
 
-
-
+        feature_df.to_csv(f"{self.output_path}/feature_df.csv", index=False)
 
 
 @app.command()
@@ -76,7 +98,9 @@ def main(
     # ----------------------------------------------
 ):
     
-    f = FeatureAbstraction(n=3,)
+    f = FeatureAbstraction(n=3, input_path=input_path, output_path=output_path)
+    
+    f.batches()
     
 
 
